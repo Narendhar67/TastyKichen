e939ccf74bd0260ae5930acca0d876f62c866fc8
Narendhar67 <narendhar.mrit467@gmail.com>
2023-07-19 09:16:10 +0000

    Links added

--- src/components/RestaurantThumbnail/index.js
+++ src/components/RestaurantThumbnail/index.js
@@ -1,29 +1,33 @@
+import {Link} from 'react-router-dom'
+
 import RatingStar from '../../images/RatingStar.png'
 
 import './index.css'
 
 const RestaurantThumbnail = props => {
   const {data} = props
-  const {name, totalRatings, rating, cuisine, imageUrl} = data
+  const {name, totalRatings, rating, cuisine, imageUrl, id} = data
 
   return (
     <li>
-      <div className="Restaurant-thumb-container">
-        <img
-          src={imageUrl}
-          alt="restaurant-logo"
-          className="restaurant-image"
-        />
-        <div className="restaurant-details-container">
-          <p className="restaurant-name">{name}</p>
-          <p className="restaurant-cuisine">{cuisine}</p>
-          <div>
-            <img src={RatingStar} alt="star" />{' '}
-            <span className="rating-thumb">{rating}</span>{' '}
-            <span className="total-ratings">({totalRatings} ratings)</span>
+      <Link to={`./restaurant/${id}`} className="link">
+        <div className="Restaurant-thumb-container">
+          <img
+            src={imageUrl}
+            alt="restaurant-logo"
+            className="restaurant-image"
+          />
+          <div className="restaurant-details-container">
+            <p className="restaurant-name">{name}</p>
+            <p className="restaurant-cuisine">{cuisine}</p>
+            <div>
+              <img src={RatingStar} alt="star" />{' '}
+              <span className="rating-thumb">{rating}</span>{' '}
+              <span className="total-ratings">({totalRatings} ratings)</span>
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
     </li>
   )
 }
